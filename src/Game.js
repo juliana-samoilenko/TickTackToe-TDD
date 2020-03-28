0608ada10a4152c1f08dbc5de4075c2218aca558
Lyashka <iuliana9864@gmail.com>
2020-03-28 18:16:53 +0300

    user move test / passed

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,16 @@
 export default class Game {
-  getState() {
-    return [
+  constructor() {
+    this._board = [
       ['', '', ''],
       ['', '', ''],
       ['', '', '']
     ]
   }
+
+  getState() {
+    return this._board;
+  }
   acceptUserMove(x, y) {
-    
+    this._board[0][0] = 'x';
   }
 }
