5cc7a4df349dedbc515bd5b28c9da39e718016f0
Lyashka <iuliana9864@gmail.com>
2020-03-28 16:26:14 +0300

    empty board test / failed

---
+++ test/game.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+
+describe('Game', () => {
+  it('Should return empty game board', () => {
+    const game = new Game();
+
+    const board = game.getState();
+
+    expect(board).to.deep.equal([
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ])
+  })
+})
+
+class Game {
+  getState() {
+    return null;
+  }
+}
