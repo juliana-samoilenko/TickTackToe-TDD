e2fd8614ef2495d419abd1601546b3edda37e025
Lyashka <iuliana9864@gmail.com>
2020-03-30 21:13:07 +0300

    test dom: empty table / passed

--- test/dom.js
+++ test/dom.js
@@ -1,31 +1,22 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
+import DomController from '../src/DomController';
 import Game from '../src/Game';
 import { isMainThread } from 'worker_threads';
 
 const {JSDOM} = jsdom;
-const dom = new JSDOM(<html><body id="root"></body></html>);
+const dom = new JSDOM('<html><body id="root"></body></html>');
+const createInstance = () => new DomController('#root');
 
 global.window = dom.window;
 global.document = dom.window.document;
 
 describe('DOM controller', () => {
   it('Create empty table', () => {
-    const domController = new DomController('#root');
+    const domController = createInstance();
 
     domController.createTable();
 
     expect(document.querySelectorAll('table').length).to.equal(1);
   })
 })
-
-
-class DomController {
-  constructor(root) {
-
-  }
-
-  createTable() {
-
-  }
-}
